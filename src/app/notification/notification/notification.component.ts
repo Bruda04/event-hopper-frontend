d1c7fac2f5b854c5c323e8925394b93b522da4e8
Vanja Kostic <vanjakostic03@gmail.com>
2024-12-02 22:44:11 +0100

    [MOD] - add notifications panel

--- src/app/notification/notification/notification.component.ts
+++ src/app/notification/notification/notification.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class NotificationComponent {
 
+    mute = false;
+
+    notifications = [
+      { title: 'Metal Head Concert CANCELED', content: 'A dialog is a type of modal window that appears in front.' },
+    { title: 'Florist “Amina” sent you a message', content: 'A dialog is a type of modal window that appears in front.' },
+    { title: 'Anika Molaw accepted your invite!', content: 'A dialog is a type of modal window that appears in front.' },
+    { title: '“Hiko” Gallery Opening Postponed', content: 'A dialog is a type of modal window that appears in front.' },
+ 
+    ];
+
+
+    toggleMute():void {
+      this.mute = !this.mute;
+    }
+
 }
