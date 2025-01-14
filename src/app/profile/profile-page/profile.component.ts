3c4b445593f043759f532ae94710335d93b954ee
marijaparezanin <marija.parezanin@mensa.ba>
2025-01-14 02:33:02 +0100

    [FIX] - Remove unused importt

--- src/app/profile/profile-page/profile.component.ts
+++ src/app/profile/profile-page/profile.component.ts
@@ -57,7 +57,8 @@ export class ProfileComponent {
           this.user.companyLocation = response.companyLocation;
           this.user.companyPhotos = response.companyPhotos;
         }
-        if(this.user.profilePicture == ""){
+        console.log(this.user.profilePicture);
+        if(this.user.profilePicture == "" || this.user.profilePicture == null){
           this.profilePicture = "profile.png";
         }else{
           this.profilePicture = environment.apiImagesHost + this.user.profilePicture;
