71bf6a37732729d097e185b04d29cfeaaba9fea5
Luka Bradic <bradicluka04@gmail.com>
2025-01-18 13:50:59 +0100

    [ADD] - Add direct chat communication on event page

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { EventCardComponent } from './event-card/event-card.component';
 import { MaterialModule } from '../infrastructure/material/material.module';
 import { EventSinglePageComponent } from './event-single-page/event-single-page.component';
+import {ChatModule} from "../chat/chat.module";
 
 
 
@@ -12,10 +13,11 @@ import { EventSinglePageComponent } from './event-single-page/event-single-page.
     EventSinglePageComponent,
     //CarouselComponent
   ],
-  imports: [
-    CommonModule,
-    MaterialModule
-  ], 
+    imports: [
+        CommonModule,
+        MaterialModule,
+        ChatModule
+    ],
   exports:[
     EventCardComponent
   ]
